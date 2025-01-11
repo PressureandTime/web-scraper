29bd5901bb5d4478dd329ccf3e1f139fbd367a83
Petar Scepanovic <petar.scepanovic@coop.co.uk>
2025-01-11 12:46:28 +0100

    update

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const HikingEventScraper = require('./scraper');
 const config = require('./config.json');
 
-const scraper = new HikingEventScraper(config);
-scraper.run()
-    .catch(error => console.error('Failed to run scraper:', error));
+const url = process.argv[2];
+if (!url) {
+  console.error('Please provide a URL to scrape');
+  process.exit(1);
+}
+
+// Create a new config with only the provided URL
+const singleUrlConfig = {
+  websites: [
+    {
+      url: url,
+      selectors:
+        config.websites.find((w) => w.url === url)?.selectors || config.websites[0].selectors,
+    },
+  ],
+};
+
+const scraper = new HikingEventScraper(singleUrlConfig);
+scraper.run().catch((error) => console.error('Failed to run scraper:', error));
